dff1ab5b87289a5c47374b087e8e3351f8e07e9b
Benjamin Feng <contact@fengb.me>
2019-12-03 01:14:58 -0600

    Compare "2" matrices instead of building one giant one

--- 03.zig
+++ 03.zig
@@ -6,7 +6,7 @@ const Pair = struct {
 };
 
 pub fn Matrix(comptime T: type, width: usize, height: usize) type {
-    return packed struct {
+    return struct {
         const Self = @This();
 
         data: [height * width]T,
@@ -56,25 +56,26 @@ fn manhattanDistance(lhs: Pair, rhs: Pair) usize {
 pub fn main() !void {
     const file = try std.fs.cwd().openRead("03.input");
 
-    var wires = try std.heap.page_allocator.create(Matrix(u8, origin.x * 2, origin.y * 2));
-    defer std.heap.page_allocator.destroy(wires);
-    wires.reset(0);
+    var wire0 = try std.heap.page_allocator.create(Matrix(u32, origin.x * 2, origin.y * 2));
+    defer std.heap.page_allocator.destroy(wire0);
+    wire0.reset(0);
 
-    var buf: [2000]u8 = undefined;
+    var line_buf: [2000]u8 = undefined;
     var file_in_stream = file.inStream();
-    var l: u8 = 1;
-    while (file_in_stream.stream.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
-        if (line == null) break;
 
+    {
+        const line0 = try file_in_stream.stream.readUntilDelimiterOrEof(line_buf[0..], '\n');
         var x: usize = origin.x;
         var y: usize = origin.y;
+        var step0: u32 = 0;
         var inner_buf: [10]u8 = undefined;
-        var line_stream = std.io.SliceInStream.init(line.?);
+        var line_stream = std.io.SliceInStream.init(line0.?);
         while (line_stream.stream.readUntilDelimiterOrEof(inner_buf[0..], ',')) |segment| {
             if (segment == null) break;
 
             var value = try std.fmt.parseUnsigned(u32, segment.?[1..], 10);
             while (value > 0) {
+                step0 += 1;
                 value -= 1;
                 switch (segment.?[0]) {
                     'R' => x += 1,
@@ -83,24 +84,43 @@ pub fn main() !void {
                     'D' => y -= 1,
                     else => unreachable,
                 }
-                wires.set(x, y, wires.get(x, y) | l);
+                wire0.set(x, y, step0);
             }
         } else |err| return err;
-
-        l <<= 1;
-    } else |err| return err;
-
-    var min: usize = std.math.maxInt(usize);
-    for (wires.data) |val, i| {
-        if (val & 0b11 == 0b11) {
-            const p = wires.pair(i);
-            const d = manhattanDistance(p, origin);
-            if (d < min) {
-                min = d;
-            }
-            std.debug.warn("({}, {}) Δ {}\n", p.x, p.y, d);
-        }
     }
 
-    std.debug.warn("Shortest: {}\n", min);
+    var min_d: usize = std.math.maxInt(usize);
+    var min_steps: usize = std.math.maxInt(usize);
+    {
+        const line1 = try file_in_stream.stream.readUntilDelimiterOrEof(line_buf[0..], '\n');
+        var x: usize = origin.x;
+        var y: usize = origin.y;
+        var steps1: u32 = 0;
+        var inner_buf: [10]u8 = undefined;
+        var line_stream = std.io.SliceInStream.init(line1.?);
+        while (line_stream.stream.readUntilDelimiterOrEof(inner_buf[0..], ',')) |segment| {
+            if (segment == null) break;
+
+            var value = try std.fmt.parseUnsigned(u32, segment.?[1..], 10);
+            while (value > 0) {
+                steps1 += 1;
+                value -= 1;
+                switch (segment.?[0]) {
+                    'R' => x += 1,
+                    'L' => x -= 1,
+                    'U' => y += 1,
+                    'D' => y -= 1,
+                    else => unreachable,
+                }
+                const steps0 = wire0.get(x, y);
+                if (steps0 > 0) {
+                    min_steps = std.math.min(min_steps, steps0 + steps1);
+                    min_d = std.math.min(min_d, manhattanDistance(origin, .{ .x = x, .y = y }));
+                }
+            }
+        } else |err| return err;
+    }
+
+    std.debug.warn("Shortest distance: {}\n", min_d);
+    std.debug.warn("Shortest steps: {}\n", min_steps);
 }
